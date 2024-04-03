e59823b722bf779ffd592a440dec6f4fc206a409
Shresth-Ojha <shresthojha.iitdelhi@gmail.com>
2024-04-03 21:43:57 +0530

    changed: reduced about us text

--- src/components/Home/About/About.jsx
+++ src/components/Home/About/About.jsx
@@ -10,15 +10,13 @@ function About() {
                 <h1 className={styles.heading}>About Us</h1>
                 <hr className={styles.horizontal_line} />
                 <p className={styles.about_description}>
-                    Nilgiri has been standing
-                    strong since 1965. Nilgiri folks aspire high, achieve
-                    high. Alumni from Nilgiri have been writing golden legacies
-                    ,be it sports or extra curricular activities or
-                    academics.Whenever it comes to street-play Nil-Bulls have
-                    always raised the bar.The best street-play performers have
-                    come from this household and joined the institute's
-                    squad.Nilgiri has been continuously dominating in sports
-                    especially Cricket and Aquatics.
+                    Nilgiri has been standing strong since 1965. Nilgiri folks
+                    aspire high, achieve high. Alumni from Nilgiri have been
+                    writing golden legacies , be it sports, extra curricular
+                    activities or academics. Whenever it comes to street-play
+                    Nil-Bulls have always raised the bar. Nilgiri has been
+                    continuously dominating in sports especially Cricket and
+                    Aquatics.
                 </p>
             </div>
         </div>
